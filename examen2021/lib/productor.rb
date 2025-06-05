f3beb8cab22f06606c9bf94c37076f77502b4cc0
Franco Alla <alu0101571669@ull.edu.es>
2025-06-05 00:00:38 +0000

    feat: Funciones de orden superior realizadas

--- examen2021/lib/productor.rb
+++ examen2021/lib/productor.rb
@@ -7,7 +7,8 @@
 #############################################################################################
 
 class Productor
-  attr_reader :id, :cultivos, :valores
+  include Comparable
+  attr_accessor :id, :cultivos, :valores
 
   def initialize(id, cultivos, valores)
     @id = id
@@ -22,4 +23,67 @@ class Productor
   def volumen_ventas
     @valores[0] * @valores[1]
   end
+
+  def <=>(other)
+    volumen_ventas() <=> other.volumen_ventas
+  end
+
+end
+
+#############################################################################################
+####################################### Pregunta 2 ##########################################
+# Una cooperativa está compuesta por un conjunto de productores. Usando programación
+# funcional, escriba el código Ruby de una función de orden superior para calcular el valor
+# mínimo del volumen de ventas de los productores que forman parte una cooperativa.
+# Describa su implementación.
+# Escriba una prueba unitaria para comprobar el correcto funcionamiento del método para
+# calcular el valor mínimo del volumen de ventas que ha desarrollado. ¿Dónde se ha de
+# almacenar el código fuente de la prueba?
+#############################################################################################
+
+def menorVolumenVentas(cooperativa)
+  cooperativa.min
+end
+
+#############################################################################################
+####################################### Pregunta 3 ##########################################
+# Escriba la prueba unitaria y el código Ruby de una función de orden superior que
+# permita incrementar en un factor el precio_unitario_venta, del volumen de ventas,
+# de los productores de una cooperativa usando programación funcional. Describa su
+# implementación.
+#############################################################################################
+
+def incrementar_factor(productor, factor)
+  productor.valores[1] += factor
+end
+
+def incrementar(cooperativa)
+  cooperativa.flat_map { |p| incrementar_factor(p, 1)}
+end
+
+# #############################################################################################
+####################################### Pregunta 4 ##########################################
+# Escriba la prueba unitaria y el código Ruby de una función de orden superior que devuelva
+# la instancia con mayor superficie total de una temporada de cultivo de los productores de una 
+# cooperativa usando programación funcional. Describa su implementación.
+#############################################################################################
+
+def sumarSuperficie(cultivos, temporada)
+  suma = 0
+  for i in 0...cultivos.size
+    if (cultivos[i][:temporada] == temporada)
+      suma += cultivos[i][:superficie]
+    end
+  end
+  suma
+end
+
+def mayorSuperficie(cooperativa, temporada)
+  cooperativa.max_by { |p| sumarSuperficie(p.cultivos, temporada)}
+end
+
+# Devolver el cultivo con mayor superficie según la temporada
+def mayorSuperficieCultivo(cooperativa, temporada)
+  cultivos = cooperativa.flat_map { |p| p.cultivos.find { |c| c[:temporada] == temporada}}
+  mayor = cultivos.max { |c| c[:superficie]}[:nombre]
 end
---
+++ examen2021/test/productor.test.rb
@@ -0,0 +1,40 @@
+require_relative '../lib/productor.rb'
+require 'test/unit'
+
+class TestProductor < Test::Unit::TestCase
+  def setup
+    @productor1 = Productor.new(1, [{:nombre => "Maiz", :superficie => 12, :temporada => "Primavera"}], [12, 5])
+    @productor2 = Productor.new(2, [
+      {:nombre => "Trigo", :superficie => 20, :temporada => "Otoño"},
+      {:nombre => "Soja", :superficie => 15, :temporada => "Verano"}
+    ], [20, 15])
+    @productor3 = Productor.new(3, [
+      {:nombre => "Cebada", :superficie => 10, :temporada => "Invierno"},
+      {:nombre => "Girasol", :superficie => 8, :temporada => "Primavera"},
+      {:nombre => "Avena", :superficie => 5, :temporada => "Otoño"}
+    ], [10, 8])
+
+    @cooperativa = [@productor1, @productor2, @productor3]
+  end
+
+  def test_menorVolumenVentas
+    assert_instance_of(Productor, menorVolumenVentas(@cooperativa))
+    assert_equal(@productor1, menorVolumenVentas(@cooperativa))
+  end
+
+  def test_incrementar
+    incrementar(@cooperativa)
+    assert_instance_of(Productor, @cooperativa[0])
+    assert_equal([12, 6], @cooperativa[0].valores)
+  end
+
+  def test_MayorSuperficie
+    assert_instance_of(Productor, mayorSuperficie(@cooperativa, "Primavera"))
+    assert_equal(@productor1, mayorSuperficie(@cooperativa, "Primavera"))
+  end
+
+  def test_nombreMayorSuperficie
+    assert_instance_of(String, mayorSuperficieCultivo(@cooperativa, "Otoño"))
+    assert_equal("Avena", mayorSuperficieCultivo(@cooperativa, "Otoño"))
+  end
+end
