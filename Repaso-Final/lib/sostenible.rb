4d65de0ace0cf93ea494f9bdd3de18eb04ed660c
Franco Alla <alu0101571669@ull.edu.es>
2025-06-09 23:23:41 +0000

    feat: Creación de hilos

--- Repaso-Final/lib/sostenible.rb
+++ Repaso-Final/lib/sostenible.rb
@@ -32,3 +32,33 @@ def mayorPrecio(cooperativa, tipo)
   precios = precios.flat_map { |a| a[:precio]}
   precios.max
 end
+
+# Cooperativa de granjas que indican sus dos valores de huellas mediante
+# hilos para hallar cada uno de sus valores
+
+granja1 = Sostenible.new(1, {carbono: 3, hidrica: 5}, [
+      {id: "Vaca", tipo: "Mamifero", precio: 3.5},
+      {id: "Cerdo", tipo: "Mamifero", precio: 2.8}
+    ])
+    granja2 = Sostenible.new(2, {carbono: 2, hidrica: 4}, [
+      {id: "Oveja", tipo: "Mamifero", precio: 2.0}
+    ])
+    granja3 = Sostenible.new(3, {carbono: 5, hidrica: 3}, [
+      {id: "Gallina", tipo: "Ave", precio: 1.5},
+      {id: "Pato", tipo: "Ave", precio: 1.8},
+      {id: "Pavo", tipo: "Ave", precio: 2.2}
+    ])
+
+@cooperativa = [granja1, granja2, granja3]
+
+threads = []
+for i in 0...@cooperativa.size
+  hilo = Thread.new do
+    puts "La granja #{@cooperativa[i].id} tiene las huellas: #{@cooperativa[i].huellas}"
+  end
+  threads.push(hilo)
+end
+
+for i in 0...threads.size
+  threads[i].join
+end
